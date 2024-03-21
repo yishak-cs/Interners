926c73a64835b6fad06fa81073ae6afaaf53e65a
yishak <yishakab24@gamil.com>
2024-03-21 15:50:36 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -25,28 +25,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/internship', [InternshipController::class, 'apiIndex']);
 Route::get('/internship/{query}', [InternshipController::class, 'apiSearchIndex']);
 
-/** department message api's */
-Route::middleware('auth:sanctum')->group(function () {
-    // department
-    Route::prefix('/department')->group(function (){
-        Route::get('/get-message/{room_id}', [MessageController::class, 'getRoomMessage']);
-        Route::get('/send-message/{room_id}', [MessageController::class, 'sendDepartmentMessage']);
-        Route::get('/get-room/{room_id}', [MessageController::class, 'getRoom']);
-        Route::get('/delete-message/{message_id}', [MessageController::class, 'deleteMessage']);
-    });
-});
-
-/** user message api's */
-Route::middleware('auth:sanctum')->group(function () {
-    // user
-    Route::prefix('/user')->group(function (){
-        Route::get('/get-message/{room_id}', [MessageController::class, 'getRoomMessage']);
-        Route::get('/send-message/{room_id}', [MessageController::class, 'sendUserMessage']);
-        Route::get('/get-room/{room_id}', [MessageController::class, 'getRoom']);
-        Route::get('/delete-message/{message_id}', [MessageController::class, 'deleteMessage']);
-    });
-});
-
 /** admin config api's */
 Route::middleware('auth:sanctum')->group(function(){
     // node
