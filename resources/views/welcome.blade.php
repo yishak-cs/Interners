6e2db350b0838e0f7d71f5d6fbb34d89d9df330e
yishak <yishakab24@gamil.com>
2024-05-12 23:10:42 +0300

    welcome page update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -61,32 +61,6 @@
     <!-- ======= End Hero Section ======= -->
 
     <main id="main">
-
-        <!-- ======= About Section ======= -->
-        <section id="about" class="about">
-            <div class="container">
-                <div class="row content">
-                    <div class="col-lg-6" data-aos="fade-right" data-aos-delay="100">
-                        <h2>About Us</h2>
-                        <h3>This web based internship applicant tracking system is a tool to manage internship
-                            applicants easly. </h3>
-                    </div>
-                    <div class="col-lg-6 pt-4 pt-lg-0" data-aos="fade-left" data-aos-delay="200">
-                        <p>
-                            Online Applicant System have:
-                        </p>
-                        <ul>
-                            <li><i class="ri-check-double-line"></i> Internship post</li>
-                            <li><i class="ri-check-double-line"></i> Manage internship applicants</li>
-                            <li><i class="ri-check-double-line"></i> Manage Universities, Companies, Facuility and
-                                Student</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <!-- ======= End About Section ======= -->
-
         <!-- ======= Count Section ======= -->
         <section id="counts" class="counts">
             <div class="container">
@@ -140,45 +114,32 @@
         </section>
         <!-- ======= End Services Section ======= -->
 
-        <!-- ======= Contact Section ======= -->
-        <section id="contact" class="contact">
+        <!-- ======= About Section ======= -->
+        <section id="about" class="about">
             <div class="container">
-                <div class="row">
-                    <div class="col-lg-4" data-aos="fade-right">
-                        <div class="section-title">
-                            <h2>Contact</h2>
-                        </div>
+                <div class="row content">
+                    <div class="col-lg-6" data-aos="fade-right" data-aos-delay="100">
+                        <h2>About Us</h2>
+                        <h3>This web based internship applicant tracking system is a tool to manage internship
+                            applicants easly. </h3>
                     </div>
-
-                    <div class="col-lg-8" data-aos="fade-up" data-aos-delay="100">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3940.3412367809183!2d38.76032677354962!3d9.03260338893554!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164b858a20bd2b09%3A0x9b593f7d0a324bfb!2zTWluaXN0cnkgb2YgRWR1Y2F0aW9uIHwgNCBraWxvIHwg4Ym14Yid4YiF4Yit4Ym1IOGImuGKkuGIteGJtOGIrSB8IDQg4Yqq4YiO!5e0!3m2!1sen!2sus!4v1711232821382!5m2!1sen!2sus" 
-                        width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                        <div class="info mt-4">
-                            <i class="bi bi-geo-alt"></i>
-                            <h4>Location:</h4>
-                            <p>Addis Abeba, Ethiopia</p>
-                        </div>
-                        <div class="row">
-                            <div class="col-lg-6 mt-4">
-                                <div class="info">
-                                    <i class="bi bi-envelope"></i>
-                                    <h4>Email:</h4>
-                                    <p>info@moe.gov.et</p>
-                                </div>
-                            </div>
-                            <div class="col-lg-6">
-                                <div class="info w-100 mt-4">
-                                    <i class="bi bi-phone"></i>
-                                    <h4>Call:</h4>
-                                    <p> +251-11-155-3133</p>
-                                </div>
-                            </div>
-                        </div>
+                    <div class="col-lg-6 pt-4 pt-lg-0" data-aos="fade-left" data-aos-delay="200">
+                        <p>
+                            Online Applicant System have:
+                        </p>
+                        <ul>
+                            <li><i class="ri-check-double-line"></i> Internship post</li>
+                            <li><i class="ri-check-double-line"></i> Manage internship applicants</li>
+                            <li><i class="ri-check-double-line"></i> Manage Universities, Companies, Facuility and
+                                Student</li>
+                        </ul>
                     </div>
                 </div>
             </div>
         </section>
-        <!-- ======= End Contact Section ======= -->
+        <!-- ======= End About Section ======= -->
+
+
 
     </main>
 
@@ -188,75 +149,89 @@
             <div class="container">
                 <div class="row">
                     <div class="col-lg-3 col-md-6 footer-contact">
-
                         <img class="logo-footer shadow-lg rounded-circle" src="assets/dist/img/logom.png"
                             alt="Logo Image" style="height: 6rem; align-items: center;">
                         <p class="pt-4">
                             FDRE Ministry of Education is a governmental organization headquartered in Addis Ababa,
-                            Arada sub-city. "
+                            Arada sub-city.
                         </p>
                     </div>
 
                     <div class="col-lg-4 col-md-6 footer-links">
                         <h3>Contact Us</h3>
                         <p>
-                        <p>
                             <i class="bi bi-geo-alt"></i>
                             Arada Sub-City, Addis Ababa
                         </p>
                         <br>
-                        <strong>E-mail</strong> : info@moe.gov.et<br>
+                        <strong>E-mail</strong>: info@moe.gov.et<br>
                         <strong>Website</strong>: www.moe.gov.et<br>
-                        <strong>Tel</strong> : +251-11-155-3133<br>
-                        </p>
+                        <strong>Tel</strong>: +251-11-155-3133<br>
                     </div>
 
-                    <div class="col-lg-4 col-md-6 footer-links">
-                        <h4>Our link</h4>
-                        <ul>
-                            <li><i class="bx bx-chevron-right"></i> <a href="{{ env('APP_URL') }}/admin/home">Admin
-                                </a></li>
-                            <li><i class="bx bx-chevron-right"></i> <a
-                                    href="{{ env('APP_URL') }}/university/home">University</a></li>
-                            <li><i class="bx bx-chevron-right"></i> <a
-                                    href="{{ env('APP_URL') }}/department/home">Department</a></li>
-                            <li><i class="bx bx-chevron-right"></i> <a href="{{ env('APP_URL') }}/user/home">User</a>
-                            </li>
-                            <li><i class="bx bx-chevron-right"></i> <a
-                                    href="{{ env('APP_URL') }}/company/home">Company</a></li>
+                    <div class="col-lg-5 col-md-6">
+                        <div class="row">
+                            <div class="col-lg-6 footer-links">
+                                <h4>Our Links</h4>
+                                <ul>
+                                    <li><i class="bx bx-chevron-right"></i> <a
+                                            href="{{ env('APP_URL') }}/admin/home">Admin</a></li>
+                                    <li><i class="bx bx-chevron-right"></i> <a
+                                            href="{{ env('APP_URL') }}/university/home">University</a></li>
+                                    <li><i class="bx bx-chevron-right"></i> <a
+                                            href="{{ env('APP_URL') }}/department/home">Department</a></li>
+                                    <li><i class="bx bx-chevron-right"></i> <a
+                                            href="{{ env('APP_URL') }}/user/home">User</a></li>
+                                    <li><i class="bx bx-chevron-right"></i> <a
+                                            href="{{ env('APP_URL') }}/company/home">Company</a></li>
+                                </ul>
+                            </div>
+
+                            <div class="col-lg-6">
+                                <iframe
+                                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3940.3412367809183!2d38.76032677354962!3d9.03260338893554!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164b858a20bd2b09%3A0x9b593f7d0a324bfb!2zTWluaXN0cnkgb2YgRWR1Y2F0aW9uIHwgNCBraWxvIHwg4Ym14Yid4YiF4Yit4Ym1IOGImuGKkuGIteGJtOGIrSB8IDQg4Yqq4YiO!5e0!3m2!1sen!2sus!4v1711232821382!5m2!1sen!2sus"
+                                    width="200" height="150" style="border:0;" allowfullscreen=""
+                                    loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                                <div class="info mt-4">
+                                    <i class="bi bi-geo-alt"></i>
+                                    <h4>Location:</h4>
+                                    <p>Addis Abeba, Ethiopia</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
 
-        <div class="container d-md-flex py-4">
+                <div class="container d-md-flex py-4">
 
-            <div class="me-md-auto text-center text-md-start">
-                <div class="copyright">
-                    &copy; Copyright <a target="_blank" href="https://moe.gov.et"><b>Ministry of Science and Higher
-                            Education</b><em></em></a>.
-                    All Rights Reserved
+                    <div class="me-md-auto text-center text-md-start">
+                        <div class="copyright">
+                            &copy; Copyright <a target="_blank" href="https://moe.gov.et"><b>Ministry of Science and
+                                    Higher
+                                    Education</b><em></em></a>.
+                            All Rights Reserved
+                        </div>
+
+                    </div>
+
+                    <div class="social-links text-center text-md-right pt-3 pt-md-0">
+                        <a href="http://twitter.com/fdremoe" target="_blank" class="twitter">
+                            <i class="bx bxl-twitter"></i>
+                        </a>
+                        <a href="https://www.facebook.com/fdremoe/" target="_blank" class="facebook">
+                            <i class="bx bxl-facebook"></i>
+                        </a>
+                        <a href="https://www.youtube.com/channel/UChrmtas-4cE6LdG1fGl91Cw" target="_blank"
+                            class="youtube">
+                            <i class="bx bxl-youtube"></i>
+                        </a>
+
+                        <a href="https://www.linkedin.com/company/ministry-of-education-ethiopia" target="_blank"
+                            class="linkedin">
+                            <i class="bx bxl-linkedin"></i>
+                        </a>
+                    </div>
                 </div>
-
-            </div>
-
-            <div class="social-links text-center text-md-right pt-3 pt-md-0">
-                <a href="http://twitter.com/fdremoe" target="_blank" class="twitter">
-                    <i class="bx bxl-twitter"></i>
-                </a>
-                <a href="https://www.facebook.com/fdremoe/" target="_blank" class="facebook">
-                    <i class="bx bxl-facebook"></i>
-                </a>
-                <a href="https://www.youtube.com/channel/UChrmtas-4cE6LdG1fGl91Cw" target="_blank" class="youtube">
-                    <i class="bx bxl-youtube"></i>
-                </a>
-
-                <a href="https://www.linkedin.com/company/ministry-of-education-ethiopia" target="_blank"
-                    class="linkedin">
-                    <i class="bx bxl-linkedin"></i>
-                </a>
-            </div>
-        </div>
     </footer>
     <!-- ======= End Footer ======= -->
 
