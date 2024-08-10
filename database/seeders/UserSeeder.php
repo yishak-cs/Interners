81a2dafe2fcde86e9e66de86240066b50e8a0372
yishak <yishakab24@gmail.com>
2024-08-11 00:47:09 +0300

    db seed

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'email' => 'admin@email.com',
+            'password' => bcrypt('12345678'),
+            'type' => 1,
+        ]);
+    }
+}
